b36a7e9823e55cfafdee33159239be8db55d1270
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-05-28 22:30:46 -0500

    add production

--- app/main.py
+++ app/main.py
@@ -17,7 +17,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hectoraliagam.vercel.app"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://hectoraliagam.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
